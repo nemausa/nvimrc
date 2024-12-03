028fb2cf70cf05a8220ba8cb76711b9070b0f574
Nemausa <tappan.billz@gmail.com>
2024-12-03 14:22:30 +0800

    fix: colorizer
    Signed-off-by: Nemausa <tappan.billz@gmail.com>

--- lua/config/colorizer.lua
+++ lua/config/colorizer.lua
@@ -1,12 +1,3 @@
-require'colorizer'.setup({
-  '*'; -- 启用所有文件类型
-}, {
-  RGB = true;
-  RRGGBB = true;
-  names = true;
-  RRGGBBAA = true;
-  rgb_fn = true;
-  hsl_fn = true;
-  css = true;
-  css_fn = true;
+require('colorizer').setup({
+  filetypes = {'*'} 
 })
