9f22c34aaa16e9f3c1dd9709b8b9393421090fa1
Nemausa <tappanmorris@outlook.com>
2024-12-29 11:31:33 +0800

    fix: dap_cpp.lua to one debuger
    Signed-off-by: Nemausa <tappanmorris@outlook.com>

--- lua/config/dap_cpp.lua
+++ lua/config/dap_cpp.lua
@@ -133,77 +133,78 @@ dap.configurations.c = {
         ignoreFailures = false,
       },
     },
-  },
-  {
-    name = "Launch gdb",
-    type = "gdb",
-    request = "launch",
-    program = function()
-      return vim.fn.input(
-        "Path to executable: ",
-        vim.fn.getcwd() .. "/",
-        "file"
-      )
-    end,
-    cwd = "${workspaceFolder}",
-    stopOnEntry = false,
-    args = {},
-    runInTerminal = true,
-  },
-  {
-    name = "Launch lldb",
-    type = "lldb",
-    request = "launch",
-    program = function()
-      return vim.fn.input(
-        "Path to executable: ",
-        vim.fn.getcwd() .. "/",
-        "file"
-      )
-    end,
-    cwd = "${workspaceFolder}",
-    stopOnEntry = false,
-    args = {},
+  }
+  -- ,
+  -- {
+  --   name = "Launch gdb",
+  --   type = "gdb",
+  --   request = "launch",
+  --   program = function()
+  --     return vim.fn.input(
+  --       "Path to executable: ",
+  --       vim.fn.getcwd() .. "/",
+  --       "file"
+  --     )
+  --   end,
+  --   cwd = "${workspaceFolder}",
+  --   stopOnEntry = false,
+  --   args = {},
+  --   runInTerminal = true,
+  -- },
+  -- {
+  --   name = "Launch lldb",
+  --   type = "lldb",
+  --   request = "launch",
+  --   program = function()
+  --     return vim.fn.input(
+  --       "Path to executable: ",
+  --       vim.fn.getcwd() .. "/",
+  --       "file"
+  --     )
+  --   end,
+  --   cwd = "${workspaceFolder}",
+  --   stopOnEntry = false,
+  --   args = {},
 
-    runInTerminal = false,
-    externalConsole = true,
-    setupCommands = {
-      text = "-enable-pretty-printing",
-      description = "Enable pretty printing",
-      ignoreFailures = false,
-    },
-  },
-  {
-    name = "Select and attach to process",
-    type = "gdb",
-    request = "attach",
-    program = function()
-      return vim.fn.input(
-        "Path to executable: ",
-        vim.fn.getcwd() .. "/",
-        "file"
-      )
-    end,
-    pid = function()
-      local name = vim.fn.input("Executable name (filter): ")
-      return require("dap.utils").pick_process({ filter = name })
-    end,
-    cwd = "${workspaceFolder}",
-  },
-  {
-    name = "Attach to gdbserver :1234",
-    type = "gdb",
-    request = "attach",
-    target = "localhost:1234",
-    program = function()
-      return vim.fn.input(
-        "Path to executable: ",
-        vim.fn.getcwd() .. "/",
-        "file"
-      )
-    end,
-    cwd = "${workspaceFolder}",
-  },
+  --   runInTerminal = false,
+  --   externalConsole = true,
+  --   setupCommands = {
+  --     text = "-enable-pretty-printing",
+  --     description = "Enable pretty printing",
+  --     ignoreFailures = false,
+  --   },
+  -- },
+  -- {
+  --   name = "Select and attach to process",
+  --   type = "gdb",
+  --   request = "attach",
+  --   program = function()
+  --     return vim.fn.input(
+  --       "Path to executable: ",
+  --       vim.fn.getcwd() .. "/",
+  --       "file"
+  --     )
+  --   end,
+  --   pid = function()
+  --     local name = vim.fn.input("Executable name (filter): ")
+  --     return require("dap.utils").pick_process({ filter = name })
+  --   end,
+  --   cwd = "${workspaceFolder}",
+  -- },
+  -- {
+  --   name = "Attach to gdbserver :1234",
+  --   type = "gdb",
+  --   request = "attach",
+  --   target = "localhost:1234",
+  --   program = function()
+  --     return vim.fn.input(
+  --       "Path to executable: ",
+  --       vim.fn.getcwd() .. "/",
+  --       "file"
+  --     )
+  --   end,
+  --   cwd = "${workspaceFolder}",
+  -- },
 }
 
 dap.configurations.cpp = dap.configurations.c
