40ae8ea79d72b30b8fa3d666a2b4eba8a360c5c2
Nemausa <tappan.billz@gmail.com>
2025-02-12 23:57:26 -0800

    feat:  使用 Ctrl + h/j/k/l 切换窗口
    Signed-off-by: Nemausa <tappan.billz@gmail.com>

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -49,3 +49,8 @@ vim.keymap.set(
   "<C-w><C-w>",
   { noremap = true, silent = true }
 )
+
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
