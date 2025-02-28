8b5bb9e807d7abc1d541ef77c58e3183dc7dccd1
Nemausa <tappan.billz@gmail.com>
2025-02-28 11:19:46 +0800

    fix: diabel h l wrap
    Signed-off-by: Nemausa <tappan.billz@gmail.com>

--- lua/options.lua
+++ lua/options.lua
@@ -19,5 +19,7 @@ vim.o.termguicolors = true
 -- vim.g.tagbar_width = 30
 -- vim.g.tagbar_position = "left"
 -- vim.g.tagbar_autofocus = 1
+
 vim.opt.makeprg = "cmake --build build"
-vim.opt.clipboard = "unnamedplus"
+vim.diagnostic.disable()
+vim.opt.whichwrap = "b,s,<,>,[,]"
