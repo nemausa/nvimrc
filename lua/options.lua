a90c12ca12ffc8f12888417cb8d094858fa9ad4f
Nemausa <tappan.billz@gmail.com>
2025-03-06 17:02:03 +0800

    change color on coc

--- lua/options.lua
+++ lua/options.lua
@@ -14,3 +14,14 @@ vim.o.clipboard = "unnamedplus"
 vim.o.termguicolors = true
 vim.diagnostic.disable()
 vim.opt.whichwrap = "b,s,<,>,[,]"
+vim.api.nvim_create_autocmd("ColorScheme", {
+    pattern = "*",
+    callback = function()
+      local bg = vim.api.nvim_get_hl_by_name("Normal", true).background
+      local fg = vim.api.nvim_get_hl_by_name("Normal", true).foreground
+      if bg and fg then
+        vim.cmd(string.format("highlight Pmenu guibg=#%06x guifg=#%06x", bg, fg))
+      end
+    end,
+  })
+  
