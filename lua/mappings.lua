ec7a1a7127d2dfeb8d3f8da3e7791d0e29a15324
Nemausa <tappan.billz@gmail.com>
2025-03-11 14:43:47 +0800

    feat: F5 run C++
    Signed-off-by: Nemausa <tappan.billz@gmail.com>

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -28,3 +28,4 @@ map("n", "<A-Down>", ":move .+1<CR>==")
 
 map("n", "<F8>", ":TagbarToggle<CR>", { noremap = true, silent = true })
 map("n", "<leader>tt", ":TagbarToggle<CR>", { noremap = true, silent = true })
+map("n", "<F5>", "<cmd>w<CR>:!g++ % -o %:r && ./%:r<CR>", { noremap = true, silent = true })
