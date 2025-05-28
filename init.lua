4909739812ccc68e28c858f68903349f11ab8d64
liwenxing <liwenxing@orvibo.com>
2025-05-28 16:31:38 +0800

    diable ctrl+s format

--- init.lua
+++ init.lua
@@ -1,3 +1,17 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git", "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",  -- 拉取最新稳定版
+    lazypath,
+  })
+end
+
+-- 2. 把它加入 runtimepath，后面才能 require("lazy")
+vim.opt.rtp:prepend(lazypath)
+
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
 vim.g.mapleader = " "
 
